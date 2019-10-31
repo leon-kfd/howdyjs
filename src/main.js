d9c7f81d75da3d76a8c3c6d2568184cfeb3082a8
fandong.kong <kfd_personal@163.com>
2019-10-31 15:53:26 +0800

    publish to local npm

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import resize from './directives/resize'
+// import resize from './directives/resize'
+import resize from 'resize-directive'
 
 Vue.config.productionTip = false
 

2d6566bcb0983efa67915ca45eef56f71d263010
leon-kfd <kfd_personal@163.com>
2020-12-28 11:07:35 +0800

    feat:standard-form

---
+++ src/howdy/packages/standard-form/src/jsx-render.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "JsxRender",
+  functional: true,
+  props: {
+    scope: Object,
+    render: Function
+  },
+  render: (h, ctx) => {
+    if (!ctx.props.render) return;
+    return ctx.props.render(h, ctx.props.scope)
+  }
+}
+</script>

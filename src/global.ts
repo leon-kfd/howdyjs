b452d86e6b4474e0bb3e72cc632f5eb0b00df9bc
leon-kfd <kfd_personal@163.com>
2021-03-15 13:35:33 +0800

    feat:change api URL

--- src/global.ts
+++ src/global.ts
@@ -1,2 +1,2 @@
-export const apiURL = (import.meta as any).env.PROD ? '../..' : '/api';
+export const apiURL = (import.meta as any).env.PROD ? '//kongfandong.cn' : '/api';
 export const baseURL = (import.meta as any).env.BASE_URL;

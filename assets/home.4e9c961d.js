989e272cdc87852cf752cea5b690a08b3d67e63d
leon-kfd <kfd_personal@163.com>
2022-01-27 09:28:52 +0000

    Deploying to gh-pages from  @ d4d68b5874f2c7c998348862b23d0922665f3774 🚀

---
+++ assets/home.4e9c961d.js
@@ -0,0 +1,82 @@
+import{R as e}from"./ReadmeFrame.53bf1e6c.js";import{_ as t}from"./index.17bab122.js";import{e as a,r as s,o as r,k as d,x as h}from"./vendor.f93cc66b.js";var i=`<h1 style="text-shadow: 3px 3px #5b9df3;color: transparent;-webkit-text-stroke-color: #262626;-webkit-text-stroke-width: 2px;font-family: unset">Howdyjs</h1>
+
+<p>
+  <a href="https://github.com/leon-kfd/howdyjs" target="_blank">
+    <img src="https://img.shields.io/github/stars/leon-kfd/howdyjs" alt="Star">
+  </a>
+  <a href="https://github.com/leon-kfd/howdyjs" target="_blank">
+    <img src="https://img.shields.io/github/forks/leon-kfd/howdyjs?color=%23cc8844" alt="Fork">
+  </a>
+  <a href="https://github.com/leon-kfd/howdyjs/blob/main/LICENSE" target="_blank">
+    <img src="https://img.shields.io/github/license/leon-kfd/howdyjs" alt="Licensee">
+  </a>
+  <img style="display: none" src="https://visitor-badge.glitch.me/badge?page_id=howdyjs.readme&amp;right_color=red" alt="Visit">
+</p>
+
+<hr>
+<p><strong>\u4E00\u4E2A\u5305\u542BJavascript\u63D2\u4EF6\u3001Vue3\u7EC4\u4EF6\u3001Vue3\u6307\u4EE4\u7684\u8F7B\u91CF\u5DE5\u5177\u7EC4\u4EF6\u5E93</strong><br><em>A universal component packages of javascript plugins or tools</em> </p>
+<blockquote>
+<p>\u5F53\u524D\u8BE5\u4ED3\u5E93\u5DF2\u4F7F\u7528<code>Vue3</code> + <code>Typescript</code>\uFF0C\u57FA\u4E8EVue2\u7EC4\u4EF6\u7684\u65E7\u7248\u9879\u76EE\u8BF7\u524D\u5F80<a href="https://github.com/leon-kfd/howdyjs/tree/howdy" target="_blank">howdy\u5206\u652F</a>\u67E5\u770B\u3002</p>
+</blockquote>
+<h2 id="link">Link</h2>
+<ul>
+<li><a href="https://github.com/leon-kfd/howdyjs" target="_blank">\u26A1Github</a></li>
+<li><a href="https://kongfandong.cn/howdy" target="_blank">\u{1F4D6}Document</a></li>
+<li><a href="https://www.npmjs.com/search?q=%40howdyjs" target="_blank">\u{1F4BE}NPM</a></li>
+</ul>
+<hr>
+<h2 id="\u7EC4\u4EF6">\u7EC4\u4EF6</h2>
+<ol>
+<li><p><strong><a href="https://kongfandong.cn/howdy/resize">@howdyjs/resize</a></strong><br>\u2728\u901A\u8FC7\u62D6\u62FD\u66F4\u6539\u5143\u7D20\u5C3A\u5BF8\u63D2\u4EF6</p>
+</li>
+<li><p><strong><a href="https://kongfandong.cn/howdy/img-zoom">@howdyjs/img-zoom</a></strong><br>\u2728\u56FE\u7247\u653E\u5927\u63D2\u4EF6\uFF0C\u652F\u6301\u6309\u7EC4\u6D4F\u89C8\u4E0B\u4E00\u5F20\u56FE\u7247</p>
+</li>
+<li><p><strong><a href="https://kongfandong.cn/howdy/size-observer">@howdyjs/size-observer</a></strong><br>\u2728\u76D1\u542C\u5143\u7D20\u5C3A\u5BF8\u53D8\u5316\u63D2\u4EF6</p>
+</li>
+<li><p><strong><a href="https://kongfandong.cn/howdy/scroll">@howdyjs/scroll</a></strong><br>\u2728\u81EA\u5B9A\u4E49\u6EDA\u52A8\u6761\u63D2\u4EF6</p>
+</li>
+<li><p><strong><a href="https://kongfandong.cn/howdy/to-drag">@howdyjs/to-drag</a></strong><br>\u2728\u8BBE\u7F6EDom\u53EF\u62D6\u52A8\u63D2\u4EF6</p>
+</li>
+<li><p><strong><a href="https://kongfandong.cn/howdy/animation-dialog">@howdyjs/animation-dialog</a></strong><br>\u2728\u52A8\u753B\u6A21\u6001\u6846Vue3\u7EC4\u4EF6</p>
+</li>
+<li><p><strong><a href="https://kongfandong.cn/howdy/standard-tabs">@howdyjs/standard-tabs</a></strong><br>\u2728\u79FB\u52A8\u7AEF\u6807\u7B7E\u9875Vue3\u7EC4\u4EF6</p>
+</li>
+<li><p><strong><a href="https://kongfandong.cn/howdy/mouse-menu">@howdyjs/mouse-menu</a></strong><br>\u2728\u81EA\u5B9A\u4E49\u53F3\u952E\u83DC\u5355Vue3\u7EC4\u4EF6</p>
+</li>
+<li><p><strong><a href="https://kongfandong.cn/howdy/to-control">@howdyjs/to-control</a></strong><br>\u2728\u62D6\u62FD\u66F4\u6539\u5143\u7D20\u5927\u5C0F\u4E0E\u4F4D\u7F6E\u63D2\u4EF6</p>
+</li>
+</ol>
+<hr>
+<h2 id="\u5F00\u59CB\u4F7F\u7528">\u5F00\u59CB\u4F7F\u7528</h2>
+<h3 id="\u5B89\u88C5">\u5B89\u88C5</h3>
+<pre><code class="language-cmd"><span class="hljs-regexp">//</span> \u65B0\u7248\u5DF2\u8FDB\u884C\u5206\u5305
+npm i -S @howdyjs/XXX
+
+<span class="hljs-regexp">//</span> \u65E7\u7248
+npm i -S howdyjs
+</code></pre>
+<h3 id="\u4F7F\u7528">\u4F7F\u7528</h3>
+<ul>
+<li>\u65B0\u7248\u8FDB\u884C\u4E86\u5206\u5305\uFF0C\u53EF\u6309\u9700\u5B89\u88C5\u76F8\u5E94\u7684\u5206\u5305</li>
+<li>\u65B0\u7248\u7684Vue\u7EC4\u4EF6\u53EA\u53EF\u5728Vue3\u4E2D\u4F7F\u7528\uFF0C\u800C\u6307\u4EE4\u505A\u4E86\u5411\u4E0B\u517C\u5BB9\u53EF\u540C\u65F6\u5728Vue2.X\u4E2D\u4F7F\u7528</li>
+<li>\u591A\u6570\u5305\u662F\u57FA\u4E8E\u539F\u751FJS\u7F16\u5199\u7684\uFF0C\u57FA\u672C\u53EF\u76F4\u63A5\u539F\u751F\u4F7F\u7528\uFF0C\u53EA\u662F\u4E3A\u4E86\u65B9\u4FBF\u63D0\u4F9B\u4E86VUE\u6307\u4EE4\u7684\u5C01\u88C5\u5F62\u5F0F</li>
+<li>\u82E5\u9700\u8981\u5728Vue2.X\u4E2D\u4F7F\u7528\uFF0C\u53EF\u4F7F\u7528\u65E7\u7248\uFF0C\u65E7\u7248\u6587\u6863 <a href="https://github.com/leon-kfd/howdyjs/blob/howdy/README.md">\u53C2\u8003\u6B64\u5904</a></li>
+</ul>
+<h2 id="\u5173\u4E8E\u9879\u76EE">\u5173\u4E8E\u9879\u76EE</h2>
+<h3 id="packages">Packages</h3>
+<ul>
+<li>\u4F7F\u7528<code>lerna</code>\u8FDB\u884C\u5206\u5305\u7BA1\u7406\uFF0C\u6267\u884C<code>npm run bootstrap</code>\u9879\u76EE\u521D\u59CB\u5316</li>
+<li>\u4F7F\u7528<code>rollup</code>\u8FDB\u884C\u6253\u5305\uFF0C \u6267\u884C<code>npm run build:pkg</code>\u6253\u5305\u5404Packages\uFF0C\u5305\u542Bcjs\u3001es\u548C\u5176d.ts\u6587\u4EF6</li>
+</ul>
+<h3 id="\u5C55\u793A\u7AD9\u70B9">\u5C55\u793A\u7AD9\u70B9</h3>
+<ul>
+<li>\u5F00\u53D1\u73AF\u5883\u53CA\u5C55\u793A\u7AD9\u70B9\u4F7F\u7528<code>vite</code>\u642D\u5EFA\uFF0Clerna\u521D\u59CB\u5316\u3001\u5B89\u88C5\u4F9D\u8D56\u3001\u6253\u5305Packages\uFF0C\u5B8C\u6210\u540E\u6267\u884C<code>npm run example</code>\u542F\u52A8</li>
+<li>\u7AD9\u70B9\u8DEF\u7531\u6587\u4EF6\u4F7F\u7528<code>import.meta.glob</code>\u8BFB\u53D6\u6587\u4EF6\u76EE\u5F55<strong>\u81EA\u52A8\u751F\u6210</strong>\uFF0C\u539F\u5219\u4E0A\u4E0D\u9700\u66F4\u6539\u8DEF\u7531\u6587\u4EF6</li>
+</ul>
+<h3 id="\u8D21\u732E\u4E0E\u672C\u5730\u5F00\u53D1">\u8D21\u732E\u4E0E\u672C\u5730\u5F00\u53D1</h3>
+<p>\u5728\u9879\u76EE\u521D\u59CB\u5316\u540E\uFF0C\u6267\u884C<code>npm run backup-package-json &amp;&amp; npm run example</code>\u53EF\u4F7F\u5C55\u793A\u7AD9\u70B9\u76F4\u63A5\u5F15\u7528\u9879\u76EE\u4E2DPackages\u7684\u4EE3\u7801\u5E76\u5B9E\u73B0\u70ED\u66F4\u65B0\uFF0C\u4F46\u6CE8\u610F\u5F00\u53D1\u5B8C\u540E\u8981\u6267\u884C<code>npm run reset-package-json</code>\u6062\u590D\u5404\u5305\u7684<strong>package.json</strong>\u3002</p>
+<p>\u539F\u7406\u662F\u66F4\u6539<code>package.json</code>\u7684main\u5B57\u6BB5\u6307\u5411\u5F00\u53D1\u7684\u6E90\u6587\u4EF6\uFF0C\u800C\u53D1\u5305\u7684\u65F6\u5019\u518D\u5C06main\u5B57\u6BB5\u5165\u53E3\u6587\u4EF6\u6307\u5411\u6253\u5305\u540E\u7684<code>index.js</code>\uFF0C\u540E\u7EED\u6709\u53EF\u80FD\u628A\u8FD9\u79CD\u65B9\u5F0F\u66F4\u6539\u4E3A\u8F6F\u8FDE\u63A5\u7684\u65B9\u5F0F\u3002</p>
+<hr>
+<p>\u6B22\u8FCE\u5404\u79CD\u5EFA\u8BAE\u53CD\u9988\u4E0EPR\uFF0C\u611F\u8C22\u4F7F\u7528\u3002</p>
+<h4 id="view-author-blog"><a href="https://www.kongfandong.cn" target="_blank" style="color: rgb(75, 9, 150)">VIEW AUTHOR BLOG</a></h4>
+`;const l=a({name:"Home",components:{ReadmeFrame:e},setup(){return{readme:i.replace(/https:\/\/kongfandong.cn\/howdy/g,"./#")}}}),p={id:"Home"};function c(n,g,m,f,u,k){const o=s("readme-frame");return r(),d("div",p,[h(o,{readme:n.readme},null,8,["readme"])])}var j=t(l,[["render",c]]);export{j as default};

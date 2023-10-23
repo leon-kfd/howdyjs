34f0cf9f43ec88f4a4a2abeecbacaf45ff36339a
kfd <kfd_personal@163.com>
2023-10-23 16:21:26 +0800

    fix: ci error

--- scripts/vue-to-md.js
+++ scripts/vue-to-md.js
@@ -9,7 +9,7 @@ classifys.map(classify => {
 const run = async () => {
   const files = await glob('src/pages/**/example/example*.vue')
   files.map(file => {
-    const codeFileName = file.replace('pages', 'code').replace('example\\', '').replace('vue', 'md');
+    const codeFileName = file.replace('pages', 'code').replace(/example[/\\]/, '').replace('vue', 'md');
     const code = fs.readFileSync(file, 'utf8');
     const output = `\`\`\`vue\n${code}\`\`\``;
     fs.writeFileSync(codeFileName, output);
